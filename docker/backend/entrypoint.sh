82f4c837fe3620e25f312ca5b5e476230dff9fd8
Siul <you@example.com>
2026-08-05 18:07:53 +0200

    seeding of the database corrected and some query work,  pushed mock-frontend for test

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -7,10 +7,15 @@ done
 
 echo "Database is ready!"
 
+npm install -D typescript tsx @types/node @prisma/adapter-pg pg @types/pg dotenv
+
+# Rigenera il Prisma Client prima di usare il client
+npx prisma generate
+
 # Esegue le migrazioni del database
 npx prisma migrate deploy
 
-# Rigenera il Prisma Client
-npx prisma generate
+# Esegue lo seed (idempotente)
+npx prisma db seed
 
 exec npm run start:dev
