4455aa42e1815d34d3cf360110918cf5627e54c1
Siul <you@example.com>
2026-09-02 17:43:06 +0200

    Just to push

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -18,7 +18,7 @@ import { APP_GUARD } from '@nestjs/core';
 @Module({
   // NELL'ARRAY SI METTE SOLO IL NOME DELLA CLASSE, NON LA STRINGA DEL PERCORSO!
   imports: [PrismaModule, AuthModule, UsersModule, RecipeModule,
-    ThrottlerModule.forRoot([{ttl: 1000, limit: 4,}]), ], 
+   ThrottlerModule.forRoot([{ttl: 100, limit: 4,}]),], 
   // gli import degli altri module creati
   controllers: [AppController], //qui ci vanno i file controller
   providers: [AppService, {provide: APP_GUARD, useClass: ThrottlerGuard,}], //qui ci vanno i file service 
