a224e701d7857d6312809cd211c5af1135f633ab
Giovanni Mercurio <gmercuri@c2r3p1.42firenze.it>
2026-09-14 17:37:54 +0200

    prettier added

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import KitchenScene from './3d/KitchenScene'
+import KitchenScene from "./3d/KitchenScene";
 
 function App() {
-	return (
-		<div className='w-screen h-screen flex bg-black'>
-			{/* Scena 3D a sinistra (70%) */}
-            <div className='w-screen h-screen'>
-                <KitchenScene />
-            </div>
-		</div>
-	)
+  return (
+    <div className="w-screen h-screen flex bg-black">
+      {/* Scena 3D a sinistra (70%) */}
+      <div className="w-screen h-screen">
+        <KitchenScene />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/frontend/src/components/RecipeCard.tsx
+++ src/frontend/src/components/RecipeCard.tsx
@@ -1,19 +1,17 @@
-
-
 interface RecipeCardProps {
-	title: string
-	difficulty: string
-	author: string
+  title: string;
+  difficulty: string;
+  author: string;
 }
 
 function RecipeCard({ title, difficulty, author }: RecipeCardProps) {
-	return (
-		<div>
-			<h2>{title}</h2>
-			<p>{difficulty}</p>
-			<p>{author}</p>
-		</div>
-	)
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{difficulty}</p>
+      <p>{author}</p>
+    </div>
+  );
 }
 
-export default RecipeCard
+export default RecipeCard;
