a30f4f342675c93ff9441af89294d4d6c2a742ad
siul <himal8438@gmail.com>
2026-09-17 00:49:46 +0200

    Nothing just some line on the signinDTO

--- src/backend/src/users/dto/signin-user.dto.ts
+++ src/backend/src/users/dto/signin-user.dto.ts
@@ -2,14 +2,6 @@ import { IsEmail, IsNotEmpty, IsOptional, IsString, IsUrl, MaxLength, MinLength}
 
 export class SignInUserDto
 {
-/*
-	@IsString()
-	@IsNotEmpty({ message: 'The username cannot be empty' })
-	@MinLength(3, { message: 'Username must be at least 3 characters long' })
-	@MaxLength(30, { message: 'Username cannot exceed 30 characters' })
-	username: string; 
-*/
-
 	@IsEmail()
 	@IsNotEmpty(({ message: 'The email can not be empty' }))
 	@MaxLength(255)
@@ -19,12 +11,4 @@ export class SignInUserDto
 	@IsString()
 	@MinLength(6, { message: 'The password must have at least 6 character' })
 	password: string
-
-/*     
-	@IsUrl()
-	@IsString()
-	@IsOptional()
-	@MaxLength(255)
-	avatar_url: string | null; 
-*/
 }
